0fb0d6a9a6c7152866fc82c077130bdf2983cde7
Sanghoon Lee <nasi3611@naver.com>
2025-03-26 10:57:03 +0900

    fix : 파베 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -5,6 +6,7 @@ import 'package:hive_flutter/adapters.dart';
 import 'constants/theme.dart';
 import 'domain/message/state/message.dart';
 import 'domain/message/view/message_list_page.dart';
+import 'firebase_options.dart';
 
 void main() async {
   try {
@@ -13,6 +15,10 @@ void main() async {
     await Hive.initFlutter();
     Hive.registerAdapter(MessageAdapter());
     await Hive.openBox<Message>('messages');
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
     runApp(const MyApp());
   } catch (e) {
